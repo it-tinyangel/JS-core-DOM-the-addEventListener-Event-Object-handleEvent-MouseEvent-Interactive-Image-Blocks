fc743063729bc725f3c36e012a4ec57c92502795
Iryna Romashko <137172921+it-tinyangel@users.noreply.github.com>
2023-11-14 22:42:46 +0200

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
-const blocks = document.querySelectorAll('.my-block');
+// Get all blocks from the DOM using the "block" class
+const blocks = document.querySelectorAll('.block');
 
 // Add a click handler for each block
 blocks.forEach((block, index) => {
 	block.addEventListener('click', function () {
-		let userURL = prompt(`Please enter the image URL for the Box ${index + 1}:`);
+		let userURL = prompt('Please enter the image URL for this Box:');
 
+		// URL validation
 		if (userURL !== null && userURL !== '') {
-			// Set the image URL as the background for the selected block
+			// Set image URL as background for selected block
 			block.style.backgroundImage = `url('${userURL}')`;
 			block.style.backgroundPosition = 'center';
 			block.style.backgroundRepeat = 'no-repeat';
 			block.style.backgroundSize = 'cover';
 		} else {
-			alert(`You did not enter the URL for the Box ${index + 1}. Please try again.`);
+			// Notify the user if they haven't entered a URL
+			alert('You did not enter the URL for this Box. Please try again.');
 		}
 	});
 });
